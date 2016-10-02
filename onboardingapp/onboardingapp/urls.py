df93f7e5b5c5aa2784222f3ebc41c7c9f71d9d0b
Den Elston <3977522@gmail.com>
2016-10-02 14:04:41 +0300

    Подключил страницу авторизации(кнопка login) с настройками allauth

--- onboardingapp/onboardingapp/urls.py
+++ onboardingapp/onboardingapp/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
+# ...
 from django.contrib import admin
-
 from .views import Homeview
 
 urlpatterns = [
+    url(r"^$", Homeview.as_view()),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('allauth.urls')),    
-    url(r"^$", Homeview.as_view()),    
 ]
