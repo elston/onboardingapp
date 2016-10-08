dec79b0005ed8180aeec5a74d4e343ce285f4f19
Den Elston <3977522@gmail.com>
2016-10-08 14:52:32 +0300

    next commit

--- onboardingapp/team/urls.py
+++ onboardingapp/team/urls.py
@@ -3,51 +3,59 @@ from django.conf.urls import url
 from team import views
 
 urlpatterns = [
+
+    # DASHBOARD
     # ...
     url(r'^dashboard$', 
         views.dashboard, 
         name='dashboard'),
 
-    # ...
+    # TEAM
+    # ...list
     url(r'^teams', 
         views.TeamsList.as_view(), 
             name='teams_list'),
 
+    # ...read
     url(r'^team/(?P<id>\d+)$', 
         views.team_info, 
             name='team_info'),
 
+    # ...create
     url(r'^team/create$', 
         views.create_team, 
             name='create_team'),
 
+    # ...update descr
     url(r'^team/edit$', 
         views.edit_team, 
             name='edit_team'),
 
+    # ...update owner
     url(r'^team/changeteamowner$', 
         views.changeteamowner_team, 
             name='changeteamowner_team'),    
+
     url(r'^team/(?P<t_id>\d+)/member/(?P<m_id>\d+)/accept-changeteamowner$',
         views.accept_changeteamowner, 
             name='accept_changeteamowner'),    
 
-    url(r'^team/limit/check$',
-        views.check_team_limit, 
-            name='check_team_limit'),
-
+    # ...delete
     url(r'^team/(?P<team_id>\d+)/delete$', 
         views.delete_team, 
             name='delete_team'),
 
-    url(r'^team/members/get-email$', 
-        views.get_members_email, 
-            name='get_members_email'),
+    # ..LIMIT
+    url(r'^team/limit/check$',
+        views.check_team_limit, 
+            name='check_team_limit'),
 
+    # ..MYTEAM
     url(r'^myteam/(?P<id>\d+)$', 
         views.myteam_info, 
             name='myteam_info'),
 
+    # ..ADDITIONAL_INFO
     url(r'^team/(?P<id>\d+)/additional-info$', 
         views.additional_info, 
             name='additional_info'),
@@ -56,12 +64,16 @@ urlpatterns = [
         views.add_additional_info, 
             name='add_additional_info'),
 
-
+    # ..MEMBERS
+    url(r'^team/members/get-email$', 
+        views.get_members_email, 
+            name='get_members_email'),
 
     url(r'^team/member/remove$', 
         views.remove_member, 
             name='remove_member'),
 
+    # ..INVITES
     url(r'^team/invite/remove$', 
         views.remove_invite, 
             name='remove_invite'),    
@@ -74,6 +86,7 @@ urlpatterns = [
         views.accept_invitation_step2, 
             name='accept_invitation_step2'),
 
+    # ..SERVICE
     url(r'^team/(?P<id>\d+)/tools$', 
         views.add_service, 
             name='add_service'),
@@ -82,6 +95,7 @@ urlpatterns = [
         views.add_service_real, 
             name='add_service_real'),
 
+    # ..ADMIN
     url(r'^team/(?P<team_id>\d+)/admin/add/(?P<user_id>\d+)$',
         views.add_admin, 
             name='add_admin'),
@@ -90,22 +104,58 @@ urlpatterns = [
         views.remove_admin, 
             name='remove_admin'),
 
-    # ....
+
+
+    # ....USER
     url(r'^user/registration/check$',
-        views.check_registration, name='check_registration'),
-    url(r'^user/invite$', views.invite_user, name='invite_user'),
-    url(r'^user/plan$', views.plan, name='plan'),
-    url(r'^user/account/update$', views.update_account, name='update_account'),
-    url(r'^user/account/charge$', views.charge_account, name='charge_account'),
-    url(r'^user/account/cancel$', views.cancel_account, name='cancel_account'),
+        views.check_registration, 
+            name='check_registration'),
 
-    url(r'^service/search$', views.search_service, name='search_service'),
+    url(r'^user/invite$', 
+        views.invite_user, 
+            name='invite_user'),
+
+    url(r'^user/plan$', 
+        views.plan, 
+            name='plan'),
+
+    url(r'^user/account/update$', 
+        views.update_account, 
+            name='update_account'),
+
+    url(r'^user/account/charge$', 
+        views.charge_account, 
+            name='charge_account'),
+
+    url(r'^user/account/cancel$', 
+        views.cancel_account, 
+            name='cancel_account'),
+
+
+    # ....SERVICE
+    url(r'^service/search$', 
+        views.search_service, 
+            name='search_service'),
+
     url(r'^service/(?P<s_name>.+)/auth-finish',
-        views.add_service_auth_finish, name='add_service_auth_finish'),
-    url(r'^service/remove$', views.remove_service, name='remove_service'),
-    url(r'^service/get-form/', views.get_form, name='get_form'),
+        views.add_service_auth_finish, 
+            name='add_service_auth_finish'),
 
-    url(r'^stripe/webhook$', views.stripe_webhook, name='stripe_webhook'),
+    url(r'^service/remove$', 
+        views.remove_service, 
+            name='remove_service'),
+
+    url(r'^service/get-form/', 
+        views.get_form, 
+            name='get_form'),
+
+    # ..STRIPE
+    url(r'^stripe/webhook$', 
+        views.stripe_webhook, 
+            name='stripe_webhook'),
+
+    # ..ERROR-LOG
     url(r'^error-log/(?P<id>\d+)/remove$',
-        views.remove_error_log, name='remove_error_log'),
+        views.remove_error_log, 
+            name='remove_error_log'),
 ]
